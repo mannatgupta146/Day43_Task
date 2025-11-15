102ab41b67a5c0c96b3c0219450cde2e644a326e
mannatgupta146 <mannatgupta146@gmail.com>
2025-11-15 19:02:40 +0530

    now working

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let arr = [
   { team: "CSK", primary: "#F9CD05", secondary: "#0D77BE", img: "./assets/csk.png" },
   { team: "MI", primary: "#004BA0", secondary: "#D1AB3E", img: "./assets/mi.png" },        
   { team: "RCB", primary: "#D71920", secondary: "#D6BD57", img: "./assets/rcb.png" },
-  { team: "KKR", primary: "#3A225D", secondary: "#D4AF37", img: "/assets/kkr.png" },
+  { team: "KKR", primary: "#3A225D", secondary: "#D4AF37", img: "./assets/kkr.png" },
   { team: "SRH", primary: "#F26522", secondary: "#000000", img: "./assets/srh.png" },
   { team: "DC", primary: "#1A3C8D", secondary: "#D71921", img: "./assets/dc.png" },
   { team: "RR", primary: "#254AA5", secondary: "#EA1A85", img: "./assets/rr.png" },  
