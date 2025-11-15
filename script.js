674b175f43cc83d81be1146880d27916ec99ea91
mannatgupta146 <mannatgupta146@gmail.com>
2025-11-15 19:00:47 +0530

    fixed

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let arr = [
-  { team: "CSK", primary: "#F9CD05", secondary: "#0D77BE", img: "/assets/csk.png" },
-  { team: "MI", primary: "#004BA0", secondary: "#D1AB3E", img: "/assets/mi.png" },        
-  { team: "RCB", primary: "#D71920", secondary: "#D6BD57", img: "/assets/rcb.png" },
+  { team: "CSK", primary: "#F9CD05", secondary: "#0D77BE", img: "./assets/csk.png" },
+  { team: "MI", primary: "#004BA0", secondary: "#D1AB3E", img: "./assets/mi.png" },        
+  { team: "RCB", primary: "#D71920", secondary: "#D6BD57", img: "./assets/rcb.png" },
   { team: "KKR", primary: "#3A225D", secondary: "#D4AF37", img: "/assets/kkr.png" },
-  { team: "SRH", primary: "#F26522", secondary: "#000000", img: "/assets/srh.png" },
-  { team: "DC", primary: "#1A3C8D", secondary: "#D71921", img: "/assets/dc.png" },
-  { team: "RR", primary: "#254AA5", secondary: "#EA1A85", img: "/assets/rr.png" },  
-  { team: "PBKS", primary: "#B92428", secondary: "#F6CD8F", img: "/assets/pbks.png" },
-  { team: "GT", primary: "#0A1A2F", secondary: "#E0C573", img: "/assets/gt.png" },
-  { team: "LSG", primary: "#00ADEF", secondary: "#375CAA", img: "/assets/lsg.png" }
+  { team: "SRH", primary: "#F26522", secondary: "#000000", img: "./assets/srh.png" },
+  { team: "DC", primary: "#1A3C8D", secondary: "#D71921", img: "./assets/dc.png" },
+  { team: "RR", primary: "#254AA5", secondary: "#EA1A85", img: "./assets/rr.png" },  
+  { team: "PBKS", primary: "#B92428", secondary: "#F6CD8F", img: "./assets/pbks.png" },
+  { team: "GT", primary: "#0A1A2F", secondary: "#E0C573", img: "./assets/gt.png" },
+  { team: "LSG", primary: "#00ADEF", secondary: "#375CAA", img: "./assets/lsg.png" }
 ];
 
 
